c496f24751d9c2f49dfb9a0da68afceddff4e609
Jean-Baptiste Landry <jblandry@absolunet.com>
2011-06-08 13:17:05 +0000

    ADD - Initial
    git-svn-id: file:///home/admabso/migration/local-svn/STRBASE/trunk@2446 e6edf6fb-f266-4316-afb4-e53d95876a76

---
+++ web/js/site.js
@@ -0,0 +1,40 @@
+/*------------------------------------------------------------------------------------//
+// Client : -
+// Date   : mois 0000
+//------------------------------------------------------------------------------------*/
+
+var MONSITE = (function(w,d,$,K,undefined){
+	
+	var variableLocale = 'yo man';
+
+	function fonctionLocale() {
+		
+	}
+	
+	
+	// ------------------------------------------
+	// PUBLIC
+	// ------------------------------------------
+	var MS = {};
+	
+	MS.variablePublique = 'bonjour monsieur';
+	
+	MS.fonctionPublique = function() {
+		
+	};
+	
+
+
+	// ------------------------------------------
+	// INIT
+	// ------------------------------------------
+	$(function(){
+	
+		// à exécuter quand le document est ready
+		
+	});
+	
+	
+	
+	return MS;
+})(window,document,jQuery,kafe);
