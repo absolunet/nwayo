ddb57ffae8af16a913ebbf4522ca8ad31c152ccc
Jb Landry <jb@jblandry.info>
2013-10-07 13:22:05 -0400

    Ajout de Drupal/Zurb

--- sources/gruntfile.js
+++ sources/gruntfile.js
@@ -38,34 +38,49 @@ module.exports = function(grunt) {
 		},
 
 
-		// less css
+		// less
 		less: {
 			core_less: {
-				files: { '../assets/builds/core-less.tmp.css': 'less/loader.less' }
+				files: { '../assets/builds/core-less.tmp.css': 'css/loader.less' }
 			},
 			editor_less: {
-				files: { '../assets/builds/editor.css': 'less/misc/editor.less' },
+				files: { '../assets/builds/editor.css': 'css/misc/editor.less' },
 				options: { yuicompress:true }
-			},
+			}
+			/**
 			newsletter_less: {
-				files: { '../assets/builds/newsletter.tmp.css': 'less/extras/newsletter.less' }
+				files: { '../assets/builds/newsletter.tmp.css': 'css/extras/newsletter.less' }
+			}
+			/**/
+		},
+		
+		/**  scss drupal-zurb
+		sass: {
+			core_sass: {
+				files: {
+					'../assets/builds/foundation-scss.tmp.css': 'css/vendor/foundation/foundation.scss'
+				}
 			}
 		},
+		/**/
+
+		// css
 		cssmin: {
 			core_css: {
 				files: {
 					'../assets/builds/core.css': [
-						'less/libs/reset.css',
-						'less/libs/normalize.css',
-						'less/libs/html5boilerplate.css',
+						'css/libs/reset.css',                           // Remove for drupal-zurb
+						'css/libs/normalize.css',
+						'css/libs/html5boilerplate.css',                // Remove for drupal-zurb
+						// '../assets/builds/foundation-scss.tmp.css',  // drupal-zurb
 						'../assets/builds/core-less.tmp.css'
 					]
-				}
+				},
 			}
 		},
 
 
-		// standalone builder
+		/** standalone builder
 		inlinecss: {
 			newsletter_html: {
 				files: {
@@ -73,6 +88,7 @@ module.exports = function(grunt) {
 				}
 			}
 		},
+		/**/
 
 
 		// cleaner
@@ -93,21 +109,32 @@ module.exports = function(grunt) {
 				tasks: 'default'
 			},
 			core_js: {
-				files: ['js/**/*.js', 'libs/**/*.js', 'tmpl/**/*.jshtml'],
+				files: [
+					'js/**/*.js',
+					'libs/**/*.js',
+					'tmpl/**/*.jshtml'
+				],
 				tasks: 'core_js'
 			},
 			core_less: {
-				files: ['less/**/*.less', 'less/**/*.css', '!less/misc/**/*.less'],
+				files: [
+					'css/**/*.css',
+					'css/**/*.less',
+					'css/**/*.scss',
+					'!css/misc/**/*'
+				],
 				tasks: 'core_less'
 			},
 			less_editor: {
-				files: ['less/misc/editor.less'],
+				files: ['css/misc/editor.less'],
 				tasks: 'editor_less'
-			},
+			}
+			/**
 			newsletter: {
-				files: ['less/misc/newsletter.less','misc/newsletter.html'],
+				files: ['css/misc/newsletter.less','misc/newsletter.html'],
 				tasks: 'newsletter'
 			}
+			/**/
 		}
 	});
 
@@ -116,19 +143,48 @@ module.exports = function(grunt) {
 	// load plugins
 	grunt.loadNpmTasks('grunt-contrib-jshint');
 	grunt.loadNpmTasks('grunt-contrib-requirejs');
-	grunt.loadNpmTasks('grunt-inline-css');
+	// grunt.loadNpmTasks('grunt-inline-css');
 	grunt.loadNpmTasks('grunt-template-client');
 	grunt.loadNpmTasks('grunt-contrib-less');
+	// grunt.loadNpmTasks('grunt-contrib-sass');  // drupal-zurb
 	grunt.loadNpmTasks('grunt-contrib-cssmin');
 	grunt.loadNpmTasks('grunt-contrib-clean');
 	grunt.loadNpmTasks('grunt-contrib-watch');
 
-	// tasks
-	grunt.registerTask('core_js',     ['templateclient:core_jshtml','jshint:core_js','requirejs:core_js','clean:builds_tmp']);
-	grunt.registerTask('core_less',   ['less:core_less','cssmin:core_css','editor_less','clean:builds_tmp']);
-	grunt.registerTask('editor_less', ['less:editor_less']);
-	grunt.registerTask('newsletter',  ['less:newsletter_less','inlinecss:newsletter_html','clean:builds_tmp']);
-	grunt.registerTask('default',     ['core_js','core_less','newsletter']);
-};
 
+
+	// tasks
+	grunt.registerTask('core_js', [
+		'templateclient:core_jshtml',
+		'jshint:core_js',
+		'requirejs:core_js',
+		'clean:builds_tmp'
+	]);
+
+	grunt.registerTask('core_less', [
+		'less:core_less',
+		//'sass:core_sass',  // drupal-zurb
+		'cssmin:core_css',
+		'editor_less',
+		'clean:builds_tmp'
+	]);
+
+	grunt.registerTask('editor_less', [
+		'less:editor_less'
+	]);
+
+	/**
+	grunt.registerTask('newsletter', [
+		'less:newsletter_less',
+		'inlinecss:newsletter_html',
+		'clean:builds_tmp'
+	]);
+	/**/
+
+	grunt.registerTask('default', [
+		'core_js',
+		'core_less'
+		//'newsletter'
+	]);
+};
 
