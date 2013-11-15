f7e7361a9eda0cf595e0a08bc6be5550a8d70dbb
Jean-Baptiste Landry <jblandry@absolunet.com>
2013-11-15 16:54:05 -0500

    Base tmpl

--- gruntfile.js
+++ gruntfile.js
@@ -2,6 +2,7 @@ module.exports = function(grunt) {
 	var
 		src        = 'src',
 		tmp        = src+'/.tmp-nwayo',
+		skeleton   = tmp+'/skeleton',
 		src_tmpl   = src+'/tmpl',
 		src_nwayo  = src+'/nwayo',
 		out_root   = './',
@@ -9,49 +10,55 @@ module.exports = function(grunt) {
 		out_doc    = 'doc',
 
 
-		tasks = { default:[/*'doc'*/] },
+		tasks = { default:[] },
 		
-		flavours = {
-			'basic':               {},
-//			'basic-foundation':    { theme:true },
-//			'drupal-basic':        { drupal:true },
-//			'drupal-foundation':   { drupal:true, foundation:true },
-//			'drupal-theme':        { drupal:true, theme:true },
-//			'magento-basic':       { magento:true },
-//			'magento-foundation':  { magento:true, foundation:true },
-//			'magento-theme':       { magento:true, theme:true },
-//			'sitecore-basic':      { sitecore:true },
-//			'sitecore-foundation': { sitecore:true, foundation:true }
-		},
+		flavours = grunt.file.readJSON(src_nwayo+'/flavours.json'),
+
+
 
 		config = {
 			pkg: grunt.file.readJSON('package.json'),
 
-			requirejs: {},
-			jshint:    {},
-			markdown:  {},
-			less:      {},
-			cssmin:    {},
-			copy:      {},
-			clean:     { build:{src: [out_build+'/**/*'],  options: { force:true }} },
-			watch:     { all: { files: ['gruntfile.js', 'package.json'], tasks: 'default' } }
+			requirejs:    {},
+			jshint:       {},
+			markdown:     {},
+			less:         {},
+			cssmin:       {},
+			copy:         {},
+			nwayo_delete: {
+				tmp:   { path:tmp },
+				build: { path:out_build }
+			},
+			nwayo_process: {},
+			watch:        { all: { files: ['gruntfile.js', 'package.json'], tasks: 'default' } }
 		},
 
-		processReadme = function(src,keep,remove) {
-			var	parts = src.split(new RegExp("{{/?"+remove+"}}",'g'));
-			for (var i=1; i<parts.length; i=i+2) {
-				parts[i] = '';
+		processTmpl = function(src,keepers,type) {
+			var delimeters = {
+				cssjs: ['/* {{','}} */'],
+				html:  ['<!-- {{','}} -->']
+			};
+
+			for (var i in keepers) {
+				src = src.replace( new RegExp(delimeters[type][0]+'/?'+keepers[i]+delimeters[type][1],'g') ,'');
 			}
-			return parts.join('').replace(new RegExp("{{/?"+keep+"}}",'g'),'');
+
+			var	parts = src.split(new RegExp(delimeters[type][0]+'/?[a-z0-9]+'+delimeters[type][1],'g'));
+			for (var j=1; j<parts.length; j=j+2) {
+				parts[j] = '';
+			}
+
+			return parts.join('');
 		}
 	;
+//	grunt.file.write(skeleton+'/'+dir[i]+'/.gitignore', '', { force:true });
 
 	grunt.template.addDelimiters('jscsscomment', '/* {%', '%} */');
 	grunt.template.addDelimiters('htmlcomment', '<!-- {%', '%} -->');
 
-	grunt.task.registerTask('nwayo_createdir', '', function(id) {
+	grunt.task.registerTask('nwayo_createtree', '', function(id) {
 		(function(dir) { for (var i in dir) {
-			grunt.file.write(out_build+'/'+id+'/'+dir[i]+'/.gitignore', '', { force:true });
+			grunt.file.write(skeleton+'/'+dir[i]+'/.gitignore', '', { force:true });
 		}})([
 			'builds',
 			'stubs',
@@ -66,10 +73,21 @@ module.exports = function(grunt) {
 		]);
 	});
 
+	grunt.task.registerMultiTask('nwayo_delete', '', function() {
+		grunt.log.writeln('Deleting '+this.data.path);
+		grunt.file.delete(this.data.path, {force:true});
+	});
+
+	grunt.task.registerMultiTask('nwayo_process', '', function() {
+		for (var i in this.data) {
+			grunt.file.write(this.data[i].src, processTmpl(grunt.file.read(this.data[i].src), this.data[i].keepers.toUpperCase(), this.data[i].type), {force:true});
+		}
+	});
+
+
 
 
 	grunt.loadNpmTasks('grunt-contrib-watch');
-	grunt.loadNpmTasks('grunt-contrib-clean');
 	grunt.loadNpmTasks('grunt-contrib-copy');
 	grunt.loadNpmTasks('grunt-markdown');
 	grunt.loadNpmTasks('grunt-contrib-jshint');
@@ -80,62 +98,109 @@ module.exports = function(grunt) {
 
 
 
+// get foundatino from github
+
+
+
+
+
+	// Build nwayo
+	config.copy.skeleton = {files: [
+		// static
+		{
+			expand: true,
+			cwd:    src_nwayo+'/',
+			src:    ['css/**/*','js/**/*','misc/**/*'],
+			dest:   skeleton+'/sources/',
+			filter: 'isFile'
+		},
+		// icons
+		{
+			expand: true,
+			cwd:    src_nwayo+'/icons',
+			src:    '**',
+			dest:   skeleton+'/sources/assets/icons/',
+			filter: 'isFile'
+		},
+		// examples
+		{
+			expand: true,
+			cwd:    src_nwayo+'/examples',
+			src:    '**',
+			dest:   skeleton+'/',
+			filter: 'isFile'
+		},
+		// grunt
+		{
+			expand: true,
+			cwd:    src_nwayo+'/grunt',
+			src:    ['*','.gitignore'],
+			dest:   skeleton+'/sources/',
+			filter: 'isFile'
+		}
+	]};
+
+	tasks.skeleton = ['nwayo_delete:build', 'nwayo_delete:tmp', 'copy:skeleton', 'nwayo_createtree'];
+	tasks.default.push('skeleton');
 
 
 
 
 	for (var id in flavours) {
 		var
-			data = flavours[id],
-			out  = out_build+'/'+id
+			data  = flavours[id],
+			local = tmp+'/'+id,
+			out   = out_build+'/'+id,
+			flags = []
 		;
 
-		// Delete build
-		config.clean[id] = { build:{src: [out+'/**/*'],  options: { force:true }} };
+		if (data.drupal)     { flags.push('drupal'); }
+		if (data.magento)    { flags.push('magento'); }
+		if (data.sitecore)   { flags.push('sitecore'); }
+		if (data.theme)      { flags.push('theme'); }
+		if (data.foundation) {
+			flags.push('foundation','responsive');
+		} else {
+			flags.push('static');
+		}
 
-		// Build nwayo
-		config.copy[id] = {files: [
-			// static
+		// copy skeleton
+		config.copy['tmp_'+id] = {
+			expand: true,
+			cwd:    skeleton+'/',
+			src:    ['**/*','**/.gitignore'],
+			dest:   tmp+'/'+id+'/',
+			filter: 'isFile'
+		};
+
+
+		// process this flavour
+		config.nwayo_process['flavour_'+id] = [
 			{
-				expand: true,
-				cwd:    src_nwayo+'/',
-				src:    ['css/**/*','js/**/*','misc/**/*'],
-				dest:   out+'/sources/',
-				filter: 'isFile'
-			},
-			// icons
-			{
-				expand: true,
-				cwd:    src_nwayo+'/icons',
-				src:    '**',
-				dest:   out+'/sources/assets/icons/',
-				filter: 'isFile'
-			},
-			// examples
-			{
-				expand: true,
-				cwd:    src_nwayo+'/examples',
-				src:    '**',
-				dest:   out+'/',
-				filter: 'isFile'
-			},
-			// grunt
-			{
-				expand: true,
-				cwd:    src_nwayo+'/grunt',
-				src:    ['*','.gitignore'],
-				dest:   out+'/sources/',
-				filter: 'isFile'
+				src:     out+'/index.html',
+				keepers: flags,
+				type:    'html'
 			}
-		]};
+		];
 
 
-		tasks['flavour_'+id] = ['clean:'+id, 'copy:'+id, 'nwayo_createdir:'+id];
+		// copy to build
+		config.copy['build_'+id] = {
+			expand: true,
+			cwd:    local+'/',
+			src:    ['**/*','**/.gitignore'],
+			dest:   out+'/',
+			filter: 'isFile'
+		};
 
-		grunt.log.writeln(out+'/**');
+
+		tasks['flavour_'+id] = ['copy:tmp_'+id, 'copy:build_'+id];
 
 		tasks.default.push('flavour_'+id);
 	}
+
+
+	tasks.default.push('nwayo_delete:tmp');
 
 
 	// --------------------------------
