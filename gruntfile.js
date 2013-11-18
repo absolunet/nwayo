60ac39b58205765571120745eae8466c83a91feb
Jean-Baptiste Landry <jblandry@absolunet.com>
2013-11-18 17:17:11 -0500

    Base preprocess

--- gruntfile.js
+++ gruntfile.js
@@ -25,36 +25,14 @@ module.exports = function(grunt) {
 			less:         {},
 			cssmin:       {},
 			copy:         {},
+			preprocess:   {},
 			nwayo_delete: {
 				tmp:   { path:tmp },
 				build: { path:out_build }
 			},
-			nwayo_process: {},
 			watch:        { all: { files: ['gruntfile.js', 'package.json'], tasks: 'default' } }
-		},
-
-		processTmpl = function(src,keepers,type) {
-			var delimeters = {
-				cssjs: ['/* {{','}} */'],
-				html:  ['<!-- {{','}} -->']
-			};
-
-			for (var i in keepers) {
-				src = src.replace( new RegExp(delimeters[type][0]+'/?'+keepers[i]+delimeters[type][1],'g') ,'');
-			}
-
-			var	parts = src.split(new RegExp(delimeters[type][0]+'/?[a-z0-9]+'+delimeters[type][1],'g'));
-			for (var j=1; j<parts.length; j=j+2) {
-				parts[j] = '';
-			}
-
-			return parts.join('');
 		}
 	;
-//	grunt.file.write(skeleton+'/'+dir[i]+'/.gitignore', '', { force:true });
-
-	grunt.template.addDelimiters('jscsscomment', '/* {%', '%} */');
-	grunt.template.addDelimiters('htmlcomment', '<!-- {%', '%} -->');
 
 	grunt.task.registerTask('nwayo_createtree', '', function(id) {
 		(function(dir) { for (var i in dir) {
@@ -78,11 +56,6 @@ module.exports = function(grunt) {
 		grunt.file.delete(this.data.path, {force:true});
 	});
 
-	grunt.task.registerMultiTask('nwayo_process', '', function() {
-		for (var i in this.data) {
-			grunt.file.write(this.data[i].src, processTmpl(grunt.file.read(this.data[i].src), this.data[i].keepers.toUpperCase(), this.data[i].type), {force:true});
-		}
-	});
 
 
 
@@ -90,6 +63,7 @@ module.exports = function(grunt) {
 	grunt.loadNpmTasks('grunt-contrib-watch');
 	grunt.loadNpmTasks('grunt-contrib-copy');
 	grunt.loadNpmTasks('grunt-markdown');
+	grunt.loadNpmTasks('grunt-preprocess');
 	grunt.loadNpmTasks('grunt-contrib-jshint');
 	grunt.loadNpmTasks('grunt-contrib-requirejs');
 	grunt.loadNpmTasks('grunt-contrib-less');
@@ -145,43 +119,32 @@ module.exports = function(grunt) {
 
 
 
+	flavours = {basic: flavours.basic};
 
 	for (var id in flavours) {
 		var
 			data  = flavours[id],
 			local = tmp+'/'+id,
-			out   = out_build+'/'+id,
-			flags = []
+			out   = out_build+'/'+id
 		;
 
-		if (data.drupal)     { flags.push('drupal'); }
-		if (data.magento)    { flags.push('magento'); }
-		if (data.sitecore)   { flags.push('sitecore'); }
-		if (data.theme)      { flags.push('theme'); }
-		if (data.foundation) {
-			flags.push('foundation','responsive');
-		} else {
-			flags.push('static');
-		}
+		data.responsive = (data.foundation);
 
 		// copy skeleton
 		config.copy['tmp_'+id] = {
 			expand: true,
 			cwd:    skeleton+'/',
 			src:    ['**/*','**/.gitignore'],
-			dest:   tmp+'/'+id+'/',
+			dest:   local+'/',
 			filter: 'isFile'
 		};
 
 
 		// process this flavour
-		config.nwayo_process['flavour_'+id] = [
-			{
-				src:     out+'/index.html',
-				keepers: flags,
-				type:    'html'
-			}
-		];
+		config.preprocess['flavour_'+id] = {
+			options: { context:data, inline:true },
+			src:     [local+'**/*.*']
+		};
 
 
 		// copy to build
@@ -194,7 +157,7 @@ module.exports = function(grunt) {
 		};
 
 
-		tasks['flavour_'+id] = ['copy:tmp_'+id, 'copy:build_'+id];
+		tasks['flavour_'+id] = ['copy:tmp_'+id, 'preprocess:flavour_'+id, 'copy:build_'+id];
 
 		tasks.default.push('flavour_'+id);
 	}
