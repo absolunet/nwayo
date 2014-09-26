771483feaa301a05838d40b2260a13604ad7e459
Jb Landry <jb@jblandry.info>
2014-09-26 16:22:30 -0400

    Base jshint

--- mockup/common/scripts/_shorcuts.js
+++ mockup/common/scripts/_shorcuts.js
@@ -1,6 +1,12 @@
+/* jshint strict: false   */
+/* exported _action       */
+/* exported _component    */
+/* exported _placeholder  */
+/* exported _field        */
+/* exported _showfor      */
 var __shortcut   = function(type,value) { return '[data-'+type+'="'+value+'"]'; };
 var _action      = function() { return __shortcut('action', arguments[0]); };
 var _component   = function() { return __shortcut('component', arguments[0]); };
 var _placeholder = function() { return __shortcut('placeholder', arguments[0]); };
-var _field = function () { return __shortcut('field', arguments[0]); };
-var _showfor = function () { return __shortcut('showfor', arguments[0]); };
+var _field       = function() { return __shortcut('field', arguments[0]); };
+var _showfor     = function() { return __shortcut('showfor', arguments[0]); };
