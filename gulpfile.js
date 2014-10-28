2b6b9da75af17fd2ebf3445aaf4cfa7912ee59b9
Jb Landry <jb@jblandry.info>
2014-10-28 11:15:10 -0400

    Base doc

---
+++ gulpfile.js
@@ -0,0 +1,8 @@
+/* jshint ignore:start */
+
+require('coffee-script/register');
+require('./doc/tasks/deploy');
+require('./doc/tasks/files');
+//require('./doc/tasks/site');
+
+/* jshint ignore:end */
