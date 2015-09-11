b76504e89baa5fea7085c6e30f5aeffcf2c9c7d0
Jb Landry <jb@jblandry.info>
2015-09-10 20:16:47 -0400

    Add PubSub pattern to base

--- src/bundles/nwayo-dependencies.js
+++ src/bundles/nwayo-dependencies.js
@@ -11,6 +11,7 @@
 	//= require bower_components/lodash/dist/lodash.min
 	//= require bower_components/underscore.string/dist/underscore.string.min
 	//= require bower_components/modernizr/modernizr
+	//= require bower_components/pubsub-js/src/pubsub
 	//= require bower_components/kafe/dist/kafe
 
 	global.nwayo = {
