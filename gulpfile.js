46a9068ff0c627eb282860b786fa63608cc95715
Jb Landry <jb@jblandry.info>
2015-10-19 10:19:12 -0400

    Remove cli

--- gulpfile.js
+++
@@ -1,8 +0,0 @@
-/* jshint ignore:start */
-
-require('coffee-script/register');
-require('./doc/tasks/deploy');
-require('./doc/tasks/files');
-//require('./doc/tasks/site');
-
-/* jshint ignore:end */
