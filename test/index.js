4e150a9bf016e015edb75b4534132b071839628d
Jb Landry <jb@jblandry.info>
2017-03-27 17:22:44 -0400

    Upgrade to new npm stack

--- test/index.js
+++ test/index.js
@@ -3,4 +3,6 @@
 //--------------------------------------------------------
 'use strict';
 
-require('./_lint-js');
+const tester = require('@absolunet/tester');
+
+tester.lintJs([...tester.ALL_JS, 'dist/!(bower_components|node_modules)/**/*.js']);
