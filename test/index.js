78efaa2cf4a7162eaad016eca2010ed6eccdb120
François Forget <fforget@absolunet.com>
2017-12-15 10:58:14 -0500

    Added vendor linting exceptions to tests

--- test/index.js
+++ test/index.js
@@ -5,4 +5,4 @@
 
 const tester = require('@absolunet/tester');
 
-tester.lintJs(['**/*.js', '!node_modules/**/*', '!boilerplate/node_modules/**/*', '!boilerplate/bower_components/**/*', '!src/**/*']);
+tester.lintJs(['**/*.js', '!node_modules/**/*', '!boilerplate/node_modules/**/*', '!boilerplate/bower_components/**/*', '!src/**/*', '!boilerplate/components/**/scripts/vendor/**/*.js']);
