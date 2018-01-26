3fcecad7dc877d877ac6a46d21a474c77c2ce56e
Jean-Baptiste Landry <jblandry@absolunet.com>
2018-01-26 08:51:55 -0500

    Rework test

--- test/index.js
+++ test/index.js
@@ -7,6 +7,22 @@ const path   = require('path');
 const slash  = require('slash');
 const tester = require('@absolunet/tester');
 
-tester.lintJs(['**/*.js', '!node_modules/**/*', '!boilerplate/node_modules/**/*', '!boilerplate/bower_components/**/*', '!boilerplate/components/**/scripts/vendor/**/*.js', '!test/fixtures/**/*.js']);
+const rootPath = slash(path.normalize(`${__dirname}/..`));
 
-tester.lintScss(['boilerplate/components/**/*.scss'], slash(path.normalize(`${__dirname}/../boilerplate/.stylelintrc.yaml`)));
+tester.lintJs([
+	`**/*.js`,
+	`!boilerplate/node_modules/**/*`,
+	`!boilerplate/bower_components/**/*`,
+	`!boilerplate/components/**/scripts/vendor/**/*.js`,
+	`!test/fixtures/**/*.js`,
+	`!test/node_modules/**/*`,
+	`!workflow/node_modules/**/*`
+], {
+	cwd: rootPath
+});
+
+
+tester.lintScss(['boilerplate/components/**/*.scss'], {
+	cwd:        rootPath,
+	configPath: slash(path.normalize(`${rootPath}/boilerplate/.stylelintrc.yaml`))
+});
