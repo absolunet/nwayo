4613bcd5f64257f3b9626d8b41044fa5c4076214
Jb Landry <jb@jblandry.info>
2018-02-21 09:33:11 -0500

    Speedup CLI

--- workflow/workflow.js
+++ workflow/workflow.js
@@ -4,18 +4,26 @@
 'use strict';
 
 const events = require('events');
+const env    = require('./helpers/env');
+
 
 //-- Boost max listeners
 events.EventEmitter.prototype._maxListeners = 100;
 
+//-- Init env
+env.initWorkflow();
 
+
+
+
+
+
+//-- Load tasks
 require('./tasks/default');
-
 require('./tasks/assets');
 require('./tasks/icons');
 require('./tasks/local');
 require('./tasks/scripts');
 require('./tasks/styles');
-
 require('./tasks/rebuild');
 require('./tasks/watch');
