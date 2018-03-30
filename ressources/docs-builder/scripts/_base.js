3750a43c45f321cd9c4cc39b0763e9aeead7eed0
Jb Landry <jb@jblandry.info>
2018-03-29 22:59:05 -0400

    Base look

--- ressources/docs-builder/scripts/_base.js
+++ ressources/docs-builder/scripts/_base.js
@@ -3,8 +3,18 @@
 //-------------------------------------
 
 (() => {
+	'use strict';
+
 	FastClick.attach(document.body);
 
+	const adjectives = [
+		'❤︎',
+		'hard work',
+		'heated debates',
+		'cold sweats',
+		'bad puns',
+		'coffee & tea'
+	];
 
-
+	document.querySelector('[data-component="adjective"]').innerHTML = adjectives[Math.floor(Math.random() * adjectives.length)];
 })();
