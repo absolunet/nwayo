38583b4a277d3fc2980e06306ce87c03ed3b4851
Jb Landry <jb@jblandry.info>
2018-06-11 16:25:00 -0400

    Doh!

--- boilerplate/components/common/scripts/_base.js
+++ boilerplate/components/common/scripts/_base.js
@@ -149,7 +149,7 @@
 
 		/**
 		// When global jQuery is ready
-		pinki.vow.then(GLOBAL_JQUERY_LOADED).then(([$Global]) => {
+		pinki.vow.when(GLOBAL_JQUERY_LOADED).then(([$Global]) => {
 			$Global().on('click');
 		});
 		/**/
@@ -194,14 +194,14 @@
 	local.subscribe();
 
 	// DOM Ready
-	pinki.vow.then(DOM_PARSED).then(() => {
+	pinki.vow.when(DOM_PARSED).then(() => {
 		local.cacheDOM();
 		local.bind();
 		local.start();
 	});
 
 	// Document loaded
-	pinki.vow.then(DOCUMENT_LOADED).then(() => {
+	pinki.vow.when(DOCUMENT_LOADED).then(() => {
 		local.delayedStart();
 	});
 
